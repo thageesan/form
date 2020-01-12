5671a9a07c8be5ba5e03b63bd13988c98fa37297
thageesan <thageesan@gmail.com>
2020-01-12 03:56:00 -0500

    - added infinite scroll

--- src/components/InfiniteImageScroll/index.js
+++ src/components/InfiniteImageScroll/index.js
@@ -1,39 +1,34 @@
 import Proptypes from 'prop-types';
 import React from 'react';
-import { Box, InfiniteScroll } from "grommet";
+import {Box, Image, InfiniteScroll} from "grommet";
+
+import './style.less'
 
 const InfiniteImageScroll = (props) => {
     const {
         items,
-        steps,
-        onMore,
     } = props;
 
     return (
-        <Box>
-            <InfiniteScroll
-                items={items}
-                step={steps}
-                onMore={onMore}>
-                {(item, index) => (
-                    <Box
-                        key={index}
-                        pad='medium'
-                        background='neutral-1'
-                        align='center'
-                    >
-                        <img src={`${process.env.CDN_DOMAIN}wedding/s_${item}`} />
-                    </Box>
-                )}
-            </InfiniteScroll>
-        </Box>
+        <InfiniteScroll
+            items={items}
+        >
+            {(item, index) => (
+                <Box
+                    key={index}
+                >
+                    <Image
+                        fit="cover"
+                        src={`${item}`}
+                    />
+                </Box>
+            )}
+        </InfiniteScroll>
     )
 }
 
 InfiniteImageScroll.propTypes = {
     items: Proptypes.array,
-    steps: Proptypes.number,
-    onMore: Proptypes.func,
 };
 
 export default InfiniteImageScroll;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Hero, InfiniteImageScroll } from './components'
+import {Hero, InfiniteImageScroll} from './components'
+import debounce from "lodash.debounce";
 
 import './style/main.less';
+import {Box} from "grommet";
 
 
 class App extends React.Component {
@@ -13,23 +15,46 @@ class App extends React.Component {
         this.state = {
             error: null,
             isLoaded: false,
-            items: null,
-            steps: 10,
-            onMore: () => {
-                console.log('!!! onMore')
-            }
+            limit: 30,
+            next: null
         };
+
+        window.onscroll = debounce( () => {
+            const {
+                error,
+                isLoaded,
+                next,
+            } = this.state;
+
+            if (error || !isLoaded || !next) return;
+
+            if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight - 900) {
+                this.getMore()
+            }
+        }, 100);
     }
 
     async componentDidMount() {
 
         try {
-            const res = await fetch('/api/v1/images?limit=10');
+            const res = await fetch(`/api/v1/images?limit=30`);
             const result = await res.json();
-            this.setState({
-                isLoaded: true,
-                items: result.images
-            });
+            const cdn_domain = process.env.CDN_DOMAIN;
+            const data = result.data;
+            if (Array.isArray(data)) {
+                const imageSets = result.data.map((result) => {
+                    return `${cdn_domain}${result.directory}/l_${result.file_name}`
+                });
+                const next = result.links.next
+                this.setState({
+                    isLoaded: true,
+                    items: [...imageSets],
+                    next: next,
+                });
+            } else {
+                throw ('Image data is not an array.')
+            }
+
         } catch (e) {
             this.setState({
                 isLoaded: true,
@@ -39,13 +64,36 @@ class App extends React.Component {
 
     }
 
+    async getMore() {
+        if (this.state.next) {
+            console.log('hit!')
+            let items = [...this.state.items];
+            const res = await fetch(`${this.state.next}`);
+            const result = await res.json();
+            const cdn_domain = process.env.CDN_DOMAIN;
+            const data = result.data;
+            const next = result.links.next;
+            if (Array.isArray(data)) {
+                const imageSets = result.data.map((result) => {
+                    return `${cdn_domain}${result.directory}/l_${result.file_name}`
+                });
+                items = items.concat(imageSets)
+                this.setState({
+                    items: [...items]
+                });
+            }
+            this.setState({
+                next: next
+            })
+        }
+
+    }
+
     render() {
         const {
             error,
             isLoaded,
             items,
-            steps,
-            onMore
         } = this.state;
 
         if (error) {
@@ -63,12 +111,13 @@ class App extends React.Component {
         } else {
             return (
                 <>
-                    <Hero />
-                    <InfiniteImageScroll
-                        items={items}
-                        step={steps}
-                        onMore={onMore()}
-                    ></InfiniteImageScroll>
+                    <Hero/>
+                    <Box className={'gallery'}>
+                        <InfiniteImageScroll
+                            items={items}
+                        >
+                        </InfiniteImageScroll>
+                    </Box>
                 </>
             )
         }
